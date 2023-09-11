fb411af015b365c6cb9119a49f18f8a2f4f92a8d
MiMiKnight <victor2015yhm@gmail.com>
2023-09-11 23:45:36 +0800

    【关联单号】US202308182020 【提交描述】全局异常处理新增处理MissingServletRequestParameterException

--- service/src/main/java/com/github/mimiknight/panda/common/aspect/HandleGlobalExceptionAspect.java
+++ service/src/main/java/com/github/mimiknight/panda/common/aspect/HandleGlobalExceptionAspect.java
@@ -3,10 +3,10 @@ package com.github.mimiknight.panda.common.aspect;
 import com.github.mimiknight.panda.model.response.ExceptionResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 /**
@@ -28,16 +28,27 @@ public class HandleGlobalExceptionAspect {
      * @param e 异常类型 {@link Exception}
      * @return {@link ExceptionResponse}<{@link ?}>
      */
+    @ResponseStatus(value = HttpStatus.INTERNAL_SERVER_ERROR)
     @ExceptionHandler(value = Exception.class)
-    public ResponseEntity<ExceptionResponse> handle(Exception e) {
-        ExceptionResponse body = ExceptionResponse.builder()
+    public ExceptionResponse handle(Exception e) {
+        return ExceptionResponse.builder()
                 .errorCode("101.F0000")
                 .errorType("System Exception")
                 .data("Default Exception").build();
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                .contentType(MediaType.APPLICATION_JSON).body(body);
-
     }
 
-
+    /**
+     * 缺失Servlet请求参数异常
+     *
+     * @param e MissingServletRequestParameterException
+     * @return {@link ExceptionResponse}
+     */
+    @ResponseStatus(value = HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(value = MissingServletRequestParameterException.class)
+    public ExceptionResponse handle(MissingServletRequestParameterException e) {
+        return ExceptionResponse.builder()
+                .errorCode("101.F0000")
+                .errorType("System Exception")
+                .data("Default Exception").build();
+    }
 }

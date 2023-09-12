1a9888e87c4b91a72a460541953e44bc2953d37e
MiMiKnight <victor2015yhm@gmail.com>
2023-09-12 22:52:06 +0800

    【关联单号】US202308182020 【提交描述】优化自定义异常

--- service/src/main/java/com/github/mimiknight/panda/common/exception/ServiceException.java
+++ service/src/main/java/com/github/mimiknight/panda/common/exception/ServiceException.java
@@ -1,14 +1,10 @@
 package com.github.mimiknight.panda.common.exception;
 
+import com.github.mimiknight.panda.common.enumeration.ErrorReturn;
 import lombok.Getter;
-import org.springframework.http.HttpStatus;
 
 /**
  * 自定义业务异常
- * <p>
- * 状态码 400：请求参数校验未通过异常
- * <p>
- * 状态码 500：服务异常
  *
  * @author victor2015yhm@gmail.com
  * @since 2023-03-10 19:04:16
@@ -18,84 +14,17 @@ public class ServiceException extends RuntimeException {
 
     private static final long serialVersionUID = -6162198998783603161L;
 
-    /**
-     * HTTP 状态码
-     */
-    private final int statusCode;
+    private final ErrorReturn errorReturn;
 
-    /**
-     * 错误码
-     */
-    private final String errorCode;
 
-    /**
-     * 错误消息
-     */
-    private final String message;
-
-    /**
-     * @param statusCode HTTP 状态码
-     * @param errorCode  错误码
-     * @param message    错误消息
-     */
-    public ServiceException(int statusCode, String errorCode, String message) {
-        super(message);
-        this.statusCode = statusCode;
-        this.errorCode = errorCode;
-        this.message = message;
+    public ServiceException(ErrorReturn errorReturn) {
+        super(errorReturn.getErrorCode() + "::" + errorReturn.getMessage());
+        this.errorReturn = errorReturn;
     }
 
-    /**
-     * @param httpStatus HTTP 状态码
-     * @param errorCode  错误码
-     * @param message    错误消息
-     */
-    public ServiceException(HttpStatus httpStatus, String errorCode, String message) {
-        this(httpStatus.value(), errorCode, message);
-    }
-
-    /**
-     * 默认状态码：500
-     *
-     * @param errorCode 错误码
-     * @param message   错误消息
-     */
-    public ServiceException(String errorCode, String message) {
-        this(HttpStatus.INTERNAL_SERVER_ERROR, errorCode, message);
-    }
-
-    /**
-     * @param statusCode HTTP 状态码
-     * @param errorCode  错误码
-     * @param message    错误消息
-     * @param cause      异常原因
-     */
-    public ServiceException(int statusCode, String errorCode, String message, Throwable cause) {
-        super(message, cause);
-        this.statusCode = statusCode;
-        this.errorCode = errorCode;
-        this.message = message;
-    }
-
-    /**
-     * @param httpStatus HTTP 状态码
-     * @param errorCode  错误码
-     * @param message    错误消息
-     * @param cause      异常原因
-     */
-    public ServiceException(HttpStatus httpStatus, String errorCode, String message, Throwable cause) {
-        this(httpStatus.value(), errorCode, message, cause);
-    }
-
-    /**
-     * 默认状态码：500
-     *
-     * @param errorCode 错误码
-     * @param message   错误消息
-     * @param cause     异常原因
-     */
-    public ServiceException(String errorCode, String message, Throwable cause) {
-        this(HttpStatus.INTERNAL_SERVER_ERROR, errorCode, message, cause);
+    public ServiceException(ErrorReturn errorReturn, Throwable cause) {
+        super(errorReturn.getErrorCode() + "::" + errorReturn.getMessage(), cause);
+        this.errorReturn = errorReturn;
     }
 
 }

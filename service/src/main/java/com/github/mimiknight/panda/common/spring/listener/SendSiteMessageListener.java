8c9607629a34dc27d105d7a836c473bcf05466ef
MiMiKnight <victor2015yhm@gmail.com>
2023-09-17 23:28:27 +0800

    【关联单号】US202308182020 【提交描述】开启异步执行Spring 监听器

--- service/src/main/java/com/github/mimiknight/panda/common/spring/listener/SendSiteMessageListener.java
+++ service/src/main/java/com/github/mimiknight/panda/common/spring/listener/SendSiteMessageListener.java
@@ -3,9 +3,11 @@ package com.github.mimiknight.panda.common.spring.listener;
 import com.github.mimiknight.panda.common.spring.event.SiteMessageEvent;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.ApplicationListener;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 
 @Slf4j
+@Async
 @Component
 public class SendSiteMessageListener implements ApplicationListener<SiteMessageEvent> {
     @Override

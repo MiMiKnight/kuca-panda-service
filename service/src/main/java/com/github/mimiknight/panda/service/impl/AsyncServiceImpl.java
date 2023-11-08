31d548cd27af41d8d921e8c0f56392b5e6f921b6
MiMiKnight <victor2015yhm@gmail.com>
2023-11-09 00:20:34 +0800

    【关联单号】US202308182020 【提交描述】增加AsyncService服务类

--- service/src/main/java/com/github/mimiknight/panda/service/impl/AsyncServiceImpl.java
+++ service/src/main/java/com/github/mimiknight/panda/service/impl/AsyncServiceImpl.java
@@ -5,13 +5,32 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Supplier;
+
 @Slf4j
 @Async
 @Service
 public class AsyncServiceImpl implements AsyncService {
 
     @Override
-    public void sendMessage(String message) {
-        log.info("message = {}", message);
+    public void run(Runnable function) {
+        function.run();
+    }
+
+    @Override
+    public <T> void run(Consumer<T> function, T value) {
+        function.accept(value);
+    }
+
+    @Override
+    public <T> T run(Supplier<T> function) {
+        return function.get();
+    }
+
+    @Override
+    public <T, R> R run(Function<T, R> function, T value) {
+        return function.apply(value);
     }
 }
--- service/src/main/java/com/github/mimiknight/panda/service/standard/AsyncService.java
+++ service/src/main/java/com/github/mimiknight/panda/service/standard/AsyncService.java
@@ -1,6 +1,16 @@
 package com.github.mimiknight.panda.service.standard;
 
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Supplier;
+
 public interface AsyncService {
 
-    void sendMessage(String message);
+    void run(Runnable function);
+
+    <T> void run(Consumer<T> function, T value);
+
+    <T> T run(Supplier<T> function);
+
+    <T, R> R run(Function<T, R> function, T value);
 }
